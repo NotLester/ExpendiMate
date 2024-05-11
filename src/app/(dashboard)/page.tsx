f7a5a8efefc87a75934e1ac99f594114a1d26b96
NotLester <lester.lewisudupi@gmail.com>
2024-05-11 14:12:57 +0530

    completed auth and navbar

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+	return (
+		<>
+			<div className="max-w-screen-2xl mx-auto">Header</div>
+		</>
+	);
+}
+
---
+++ src/components/header-logo.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export function HeaderLogo() {
+	return (
+		<Link href="/">
+			<div className="items-center hidden lg:flex">
+				<Image src="/logo.svg" alt="Logo" width={28} height={28} />
+				<p className="font-semibold text-white text-2xl ml-2.5">ExpendiMate</p>
+			</div>
+		</Link>
+	);
+}
+
